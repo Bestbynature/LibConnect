0462846b1f404879b21ef91707406d80745ad08d
Damilare <bestbynature2010@gmail.com>
2023-06-19 11:04:22 +0100

    :zap: Add the trim decorator class

---
+++ trim_decor.rb
@@ -0,0 +1,8 @@
+class TrimmerDecorator < BaseDecorator
+  MAX_LENGTH = 10
+
+  def correct_name
+    name = @nameable.correct_name
+    name.length > MAX_LENGTH ? name[0...MAX_LENGTH] : name
+  end
+end
