17350fdf41d85748e8b9c7bdff41e3b38890fac7
Damilare <bestbynature2010@gmail.com>
2023-06-19 11:05:36 +0100

    :zap: Modify the file structures to contain one class per file

--- nameable.rb
+++ nameable.rb
@@ -14,18 +14,3 @@ class BaseDecorator < Nameable
     @nameable.correct_name
   end
 end
-
-class CapitalizeDecorator < BaseDecorator
-  def correct_name
-    @nameable.correct_name.upcase
-  end
-end
-
-class TrimmerDecorator < BaseDecorator
-  MAX_LENGTH = 10
-
-  def correct_name
-    name = @nameable.correct_name
-    name.length > MAX_LENGTH ? name[0...MAX_LENGTH] : name
-  end
-end
